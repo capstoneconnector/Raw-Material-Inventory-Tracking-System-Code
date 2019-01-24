2788e57d661284cfb1588f3422c61b3ed0e11c0e
bwprescott <blakewillprescott@gmail.com>
2019-01-24 10:52:00 -0600

    Update urls.py
    The wrong value was shown in material instances, fixed by replacing the variable.

--- InventorySystem/urls.py
+++ InventorySystem/urls.py
@@ -3,7 +3,7 @@ from .views import index, material_instances, total_amounts, material_total_amou
 
 urlpatterns = [
     path('', index),
-    path('material/instances/<str:materialName>', material_total_amount),
+    path('material/instances/<str:materialName>', material_instances),
     path('material/total/<str:materialName>', material_total_amount),
     path('material/', total_amounts)
 ]
