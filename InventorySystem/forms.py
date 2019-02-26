cc67d2541746b7b4a214854c85e17b041ce521d8
Evan Fischer <erfischer2@bsu.edu>
2019-02-26 02:03:02 -0500

    Added Updated By and Last Updated along with fixing redirect on pages.

--- InventorySystem/forms.py
+++ InventorySystem/forms.py
@@ -1,5 +1,6 @@
 from .models import Material, MaterialType
 from django.forms import ModelForm
+from django import forms
 
 
 
@@ -7,8 +8,10 @@ class MaterialForm(ModelForm):
     class Meta:
         model = Material
         fields = '__all__'
+        widgets = {'material_type': forms.HiddenInput()}
 
 class MaterialTypeForm(ModelForm):
     class Meta:
         model = MaterialType
         fields = '__all__'
+        #widgets = {'date_updated': forms.HiddenInput()}
